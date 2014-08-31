5b92a7cf00c69a88cdc98495222e9b4aca3d41b3
Laszlo Solova <laszlo@solova.com>
2014-08-31 10:32:23 +0200

    Upgrade database: use only minutes instead of own hours-minutes format.

--- js/app/MeasureStorage.js
+++ js/app/MeasureStorage.js
@@ -15,15 +15,29 @@ define(['mt', 'tu'], function (MeasureTime, TimeUtils) {
             store.setItem('firstday', TimeUtils.asDay(firstRemoveTime));
         },
         upgrade = function(fromVersion, toVersion) {
-            for (var i = fromVersion; i < toVersion; i++) {
+            var day, dayTime, dayTimeLength, i, j, upgradedTime
+                ;
+            for (i = fromVersion; i < toVersion; i++) {
                 switch (i) {
+                    case 1 :
+                        for (j = 70; j > 0; j--) {
+                            day = TimeUtils.asDay(Date.now() - (j * 86400000));
+                            dayTime = store.getItem(day);
+                            if (dayTime !== null) {
+                                dayTimeLength = dayTime.length;
+                                upgradedTime = (2 < dayTimeLength ? parseInt(dayTime.substring(0, dayTimeLength - 2)) * 60 : 0) + parseInt(dayTime.substring(dayTimeLength - 2));
+                                console.log(dayTime + " converted to " + upgradedTime);
+                                store.setItem(day, upgradedTime);
+                            }
+                        };
+                        break;
                     default:
                         break;
                 }
             }
         },
         dbversion,
-        requiredDbVersion = 1,
+        requiredDbVersion = 2,
         MeasureStorage = function () {
             dbversion = this.get('dbversion');
             if (dbversion !== null) {
@@ -50,7 +64,7 @@ define(['mt', 'tu'], function (MeasureTime, TimeUtils) {
     MeasureStorage.prototype.getTimeOfDay = function (day) {
         var dayValue = store.getItem(day);
         if (dayValue === null || dayValue === undefined) {
-            return MeasureTime.create(day, "000");
+            return MeasureTime.create(day, 0);
         }
         return MeasureTime.create(day, dayValue);
     };
--- js/app/MeasureTime.js
+++ js/app/MeasureTime.js
@@ -1,55 +1,42 @@
 ﻿"use strict";
 define(['tu'], function (TimeUtils) {
-    var MeasureTime = function () {
-        this._day = TimeUtils.asDay(Date.now());
-        this._hours = "0";
-        this._minutes = "0";
-    };
+    return {
+        create: function(day, minutes) {
+            var MeasureTime = function (day, minutes) {
+                this.day = day || TimeUtils.asDay(Date.now());
+                this.minutes = parseInt(minutes, 10) || 0;
+            }
+            MeasureTime.prototype.getDay = function getDay() {
+                return parseInt(TimeUtils.reduce(this.day.substring(6,8)), 10);
+            };
 
-    MeasureTime.create = function (day, time) {
-        var mTimeInstance = new MeasureTime();
-        mTimeInstance._day = day;
-        var hourLength = 1;
-        if (time.length === 4) {
-            hourLength = 2;
+            MeasureTime.prototype.getYearAndMonth = function () {
+                return this.day.substring(0, 4) + "/" + this.day.substring(4, 6);
+            };
+
+            MeasureTime.prototype.getFullDay = function () {
+                return this.day;
+            };
+
+            MeasureTime.prototype.getMinutes = function () {
+                return this.minutes;
+            };
+
+            MeasureTime.prototype.getFormattedTime = function () {
+                var fHours = Math.floor(this.minutes/60),
+                    fMinutes = TimeUtils.extend(this.minutes%60);
+                return fHours + ":" + fMinutes;
+            };
+
+            MeasureTime.prototype.getRecordTime = function () {
+                return this.minutes;
+            };
+
+            MeasureTime.prototype.increment = function (addMinutes) {
+                this.minutes += addMinutes;
+            };
+            return new MeasureTime(day, minutes);
         }
-        mTimeInstance._hours = time.substring(0, hourLength);
-        mTimeInstance._minutes = TimeUtils.reduce(time.substring(hourLength));
-        return mTimeInstance;
-    };
+    }
 
-    MeasureTime.prototype.getDay = function () {
-        return parseInt(TimeUtils.reduce(this._day.substring(6,8)), 10);
-    };
-
-    MeasureTime.prototype.getYearAndMonth = function () {
-        return this._day.substring(0, 4) + "/" + this._day.substring(4, 6);
-    };
-
-    MeasureTime.prototype.getFullDay = function () {
-        return this._day;
-    };
-
-    MeasureTime.prototype.getMinutes = function () {
-        return parseInt(this._hours, 10) * 60 + parseInt(this._minutes, 10);
-    };
-
-    MeasureTime.prototype.getFormattedTime = function () {
-        return this._hours + ":" + TimeUtils.extend(this._minutes);
-    };
-
-    MeasureTime.prototype.getRecordTime = function () {
-        return "" + this._hours + TimeUtils.extend(this._minutes);
-    };
-
-    MeasureTime.prototype.increment = function (addMinutes) {
-        var newMinutes = parseInt(this._minutes, 10) + addMinutes;
-        while (60 <= newMinutes) {
-            newMinutes = newMinutes-60;
-            this._hours++;
-        }
-        this._minutes = newMinutes;
-    };
-
-    return MeasureTime;
 });
