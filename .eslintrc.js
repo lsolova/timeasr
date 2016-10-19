aa036863d9a730c671c005425b938e14b50ffb32
Laszlo Solova <laszlo@solova.com>
2016-10-19 19:02:14 +0200

    Set up initial eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+    'extends': 'eslint:recommended',
+    'rules': {
+        'curly': 'error',
+        'radix': 'error',
+        'vars-on-top': 'error',
+        'wrap-iife': 'error',
+        'max-len': ['error', {'code': 120, 'ignoreUrls': true}]
+    },
+    'env': {
+        'es6': true,
+        'node': true
+    }
+}
---
+++ public/.eslintrc.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+    'env': {
+        'amd': true,
+        'browser': true
+    }
+}
