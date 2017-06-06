1eb7e7fe045a62dc56cd594e4a89418781181e42
Laszlo Solova <laszlo@solova.com>
2017-06-06 15:55:04 +0200

    Set up Karma testing.

---
+++ .config/karma.conf.js
@@ -0,0 +1,17 @@
+var webpackConfig = require('./webpack.config');
+webpackConfig.devtool = 'inline-source-map';
+
+module.exports = function (config) {
+    config.set({
+        basePath: '..',
+        frameworks: ['mocha'],
+        files: [
+            'src/**/*.test.js'
+        ],
+        preprocessors: {
+            'src/**/*.test.js': ['webpack', 'sourcemap']
+        },
+        reporters: ['mocha'],
+        webpack: webpackConfig
+    });
+}
