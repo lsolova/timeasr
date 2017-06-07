a9ef9a965c7adabc7c56efe23263da3d29603d73
Laszlo Solova <laszlo@solova.com>
2017-06-07 19:38:47 +0200

    Move time calculations from MeasureController.
    MeasureController is not included in this commit due to other refactorings.

--- src/client/js/utils/timeCalculation.js
+++ src/client/js/utils/timeCalculation.js
@@ -2,6 +2,25 @@ import { asMinutes } from './timeConversion';
 
 const timeregex = /^[+]?([-]?\d{1,2}:\d{2}) ?.*$/;
 
+function calculateStatistics(measuredTimes, loopCalculation, postCalculation) {
+    let measuredTimeObj = { dayCount: 0, statTime: 0 };
+
+    if (measuredTimes && typeof measuredTimes.reduce === 'function') {
+        measuredTimeObj = measuredTimes.reduce(function (accObj, measuredTime) {
+            if (0 < measuredTime.getMinutes()) {
+                accObj.statTime += loopCalculation(measuredTime);
+                accObj.dayCount++;
+            }
+            return accObj;
+        }, measuredTimeObj);
+    }
+
+    return {
+        statValue: postCalculation(measuredTimeObj.statTime, measuredTimeObj.dayCount),
+        statCount: measuredTimeObj.dayCount
+    };
+}
+
 export function calculateMonthlyAdjustmentFromDetails(valueList) {
     let sumValue = 0;
     if (valueList) {
@@ -17,4 +36,38 @@ export function calculateMonthlyAdjustmentFromDetails(valueList) {
             }, 0);
     }
     return sumValue;
+}
+
+export function calculateMonthlyAverage(measuredTimes, monthlyAdjustment) {
+    return calculateStatistics(measuredTimes
+        , function (measuredTime) {
+            return measuredTime.getMinutes();
+        }
+        , function (statTime, dayCount) {
+            return dayCount === 0 ? 0 : Math.floor((statTime + monthlyAdjustment) / dayCount);
+        }
+    );
+}
+
+export function calculateMonthlyDifference(measuredTimes, monthlyAdjustment = 0, expectedDayTime = 0) {
+    return calculateStatistics(measuredTimes
+        , function (measuredTime) {
+            return measuredTime.getMinutes() - expectedDayTime;
+        }
+        , function (statTime) {
+            return statTime + monthlyAdjustment;
+        }
+    );
+}
+
+export function estimateLeavingTime(dailyTime = 0, differenceTime = 0, expectedDayTime) {
+    let leavingTime = 0;
+    if (dailyTime >= 0) {
+        let calcTime = expectedDayTime - (dailyTime + differenceTime);
+        if (calcTime > 0) {
+            let todayDate = new Date();
+            leavingTime = todayDate.getHours() * 60 + todayDate.getMinutes() + calcTime;
+        }
+    }
+    return leavingTime;
 }
--- src/client/js/utils/timeCalculation.test.js
+++ src/client/js/utils/timeCalculation.test.js
@@ -34,4 +34,49 @@ describe('Time calculation utils', function () {
                 .should.equal(30);
         });
     });
+
+    describe('#calculateMonthlyAverage', function () {
+        it('with no data', function () {
+            timeUtils.calculateMonthlyAverage().should.deep.equal({ statValue: 0, statCount: 0 });
+        });
+
+        it('with simple data', function () {
+            timeUtils.calculateMonthlyAverage([createMeasureTimeObject(12), createMeasureTimeObject(36)], 0)
+                .should.deep.equal({ statValue: 24, statCount: 2 });
+        });
+
+        it('with adjustment', function () {
+            timeUtils.calculateMonthlyAverage([createMeasureTimeObject(24), createMeasureTimeObject(36)], -12)
+                .should.deep.equal({ statValue: 24, statCount: 2 });
+        });
+    });
+
+    describe('#calculateMonthlyDifference', function () {
+        it('with no data', function () {
+            timeUtils.calculateMonthlyDifference()
+                .should.deep.equal({ statValue: 0, statCount: 0 });
+        });
+
+        it('with simple data', function () {
+            timeUtils.calculateMonthlyDifference([createMeasureTimeObject(12), createMeasureTimeObject(36)], 0, 20)
+                .should.deep.equal({ statValue: 8, statCount: 2 });
+        });
+
+        it('with adjustment', function () {
+            timeUtils.calculateMonthlyDifference([createMeasureTimeObject(24), createMeasureTimeObject(36)], -12, 20)
+                .should.deep.equal({ statValue: 8, statCount: 2 });
+        });
+    });
+
+    describe('#estimateLeavingTime', function () {
+        xit('', function () { });
+    });
 });
+
+function createMeasureTimeObject(value) {
+    return {
+        getMinutes: function () {
+            return value;
+        }
+    };
+}
