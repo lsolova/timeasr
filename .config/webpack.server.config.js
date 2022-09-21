4e40d1a3d02dedfe73cdcf13eb0d4e2b0be3f5e9
Laszlo Solova <laszlo@solova.com>
2022-09-21 17:04:48 +0200

    Improve linting and tooling - Add SVGR to support inline SVG in React components - Webpack configuration changes - Linting cleaned for ES, Markdown, Style - Initial TS config created - Include VSCode custom settings and extension recommendations

--- .config/webpack.server.config.js
+++ .config/webpack.server.config.js
@@ -3,6 +3,7 @@ const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common.config');
 
 const serverConfig = merge(commonConfig, {
+    mode: 'production',
     target: 'node',
     entry: {
         server: './src/scripts/server.js'
