1b9a71d5759e706b97839a6a3b911c2461357588
Laszlo Solova <laszlo@solova.com>
2022-12-08 14:06:10 +0100

    - Fix scrolling of measure list - Fix placement of SolovaCOM logo - Update git ignores

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
+# Timeasr
+
 [![Azure Static Web Apps CI/CD](https://github.com/lsolova/timeasr/actions/workflows/azure-static-web-apps-agreeable-field-042a6af03.yml/badge.svg)](https://github.com/lsolova/timeasr/actions/workflows/azure-static-web-apps-agreeable-field-042a6af03.yml)
 
-# Timeasr
 Simple worktime measurement tool
+
+## Sources
+
+* Some icons: <https://www.svgrepo.com/collection/ikonate-thin-interface-icons/>
--- src/components/measure-list.svelte
+++ src/components/measure-list.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
     import { Task } from "../types";
-    import { tasks } from "./stores";
+    import { hideTask, tasks } from "./stores";
     import { TimeasrStore } from "../logic/timeasr-store";
     import { toHoursAndMinutes } from "../logic/time-formatters";
     import AddTask from "./add-task.svelte";
-    import SolovaLogoSvg from "./svg/solova-logo-svg.svelte";
+    import EyeCrossedSvg from "./svg/eye-crossed-svg.svelte";
 
     const classNames = (task: Task) => {
         const cn = ["MeasureList__Item"];
@@ -13,20 +13,29 @@
         }
         return cn.join(" ");
     }
-    const onClick = (task: Task) => {
+    const onSelectClick = (task: Task) => {
         task.active ? TimeasrStore.closeTimelog() : TimeasrStore.startTimelog(task.name);
+    }
+    const onHideClick = (task: Task) => {
+        hideTask(task)
     }
 </script>
 
 <main class="MeasureList">
     {#each $tasks as task}
-        <div class={classNames(task)} on:click={() => onClick(task)}>
+        <div class={classNames(task)} on:click={() => onSelectClick(task)}>
+            {#if !task.active}
+                <button class="MeasureList__Item__Close" on:click={(event) => {
+                    event.preventDefault();
+                    event.stopImmediatePropagation();
+                    onHideClick(task);
+                }}>
+                    <EyeCrossedSvg />
+                </button>
+            {/if}
             <span>{task.name}</span>
             <span>{toHoursAndMinutes(task.loggedTime)}</span>
         </div>
     {/each}
     <AddTask />
-    <div class="Copyright">
-        <SolovaLogoSvg />
-    </div>
 </main>
